1d2269735f12f194ef0ebc62a7066d99f9c7067c
H. Vlaardingerbroek <hannes@vlaardingerbroek.nl>
2018-01-22 11:29:47 +0100

    Clean up comments

--- constants.py
+++ constants.py
@@ -41,29 +41,6 @@ class SyrNT:
         ('Rev',    (20, 29, 22, 11, 14, 17, 17, 13, 21, 11, 19, 17, 18, 20,
                     8, 21, 18, 24, 21, 15, 27, 20)))
 
-    # TODO check discrepancies between SEDRA and Syromorph NT.
-    # It seems that
-    # 521601715
-    # 550104214
-    # 562103016
-    # 562302606
-    # 580201122
-    # 781901718
-    # 782202101
-    # 782202102
-    # 782202103
-    # 782202104
-    # 782202105
-    # 782202106
-    # 782202107
-    # 782202108
-
-    # NT_VERSES tuple replaced by generator: get_verse_labels()
-    # NT_VERSES = tuple( (book_id, book_name, chapter, verse)
-    #     for book_id, (book_name, chapters) in enumerate(NT_BOOKS, 52)
-    #         for chapter, versecount in enumerate(chapters.split(), 1)
-    #             for verse in range(1, int(versecount) + 1) )
-
     # SyroMorph Data format
     #
     # See also morph.app.SyriacDatum.java,
@@ -76,165 +53,138 @@ class SyrNT:
     # separated by a space.
     #
     # The annotations are as follows:
-    # <index> <name>
-    #  0 stem
-    #  1 lexeme (baseform)
-    #  2 root
-    #  3 prefix
-    #  4 suffix
-    #  5 seyame
-    #  6 verb conjugation
-    #  7 aspect
-    #  8 state
-    #  9 number
-    # 10 person
-    # 11 gender
-    # 12 pronoun type
-    # 13 demonstrative category
-    # 14 noun type
-    # 15 numeral type
-    # 16 participle type
-    # 17 grammatical category
-    # 18 suffix contraction
-    # 19 suffix gender
-    # 20 suffix person
-    # 21 suffix number
-    # 22 feminine he dot
-    #
-    # The tagsets for annotations 5 through 22 are described below:
-    #
-    ANNOTATIONS = (
-        ('stem', None),
-        ('lexeme', None),
-        ('root', None),
-        ('prefix', None),
-        ('suffix', None),
-        ('seyame', None),
-        ('verbal_conjugation',  # verbal_conjugation
-            ('n/a',             # 0 n/a
-             'peal',            # 1 peal
-             'ethpeal',         # 2 ethpeal
-             'pael',            # 3 pael
-             'ethpael',         # 4 ethpael
-             'aphel',           # 5 aphel
-             'ettaphal',        # 6 ettaphal
-             'shaphel',         # 7 shaphel
-             'eshtaphal',       # 8 eshtaphal
-             'saphel',          # 9 saphel
-             'estaphal',        # 10 estaphal
-             'pauel',           # 11 pauel
-             'ethpaual',        # 12 ethpaual
-             'paiel',           # 13 paiel
-             'ethpaial',        # 14 ethpaial
-             'palpal',          # 15 palpal
-             'ethpalpal',       # 16 ethpalpal
-             'palpel',          # 17 palpel
-             'ethpalpal2',      # 18 ethpalpal2
-             'pamel',           # 19 pamel
-             'ethpamel',        # 20 ethpamel
-             'parel',           # 21 parel
-             'ethparal',        # 22 ethparal
-             'pali',            # 23 pali
-             'ethpali',         # 24 ethpali
-             'pahli',           # 25 pahli
-             'ethpahli',        # 26 ethpahli
-             'taphel',          # 27 taphel
-             'ethaphal')        # 28 ethaphal
+    ANNOTATIONS = (             # <index> <name>
+        ('stem', None),         #  0 stem
+        ('lexeme', None),       #  1 lexeme (baseform)
+        ('root', None),         #  2 root
+        ('prefix', None),       #  3 prefix
+        ('suffix', None),       #  4 suffix
+        ('seyame', None),       #  5 seyame
+        ('verbal_conjugation',  #  6 verb conjugation
+            ('n/a',                 #  0 n/a
+             'peal',                #  1 peal
+             'ethpeal',             #  2 ethpeal
+             'pael',                #  3 pael
+             'ethpael',             #  4 ethpael
+             'aphel',               #  5 aphel
+             'ettaphal',            #  6 ettaphal
+             'shaphel',             #  7 shaphel
+             'eshtaphal',           #  8 eshtaphal
+             'saphel',              #  9 saphel
+             'estaphal',            # 10 estaphal
+             'pauel',               # 11 pauel
+             'ethpaual',            # 12 ethpaual
+             'paiel',               # 13 paiel
+             'ethpaial',            # 14 ethpaial
+             'palpal',              # 15 palpal
+             'ethpalpal',           # 16 ethpalpal
+             'palpel',              # 17 palpel
+             'ethpalpal2',          # 18 ethpalpal2
+             'pamel',               # 19 pamel
+             'ethpamel',            # 20 ethpamel
+             'parel',               # 21 parel
+             'ethparal',            # 22 ethparal
+             'pali',                # 23 pali
+             'ethpali',             # 24 ethpali
+             'pahli',               # 25 pahli
+             'ethpahli',            # 26 ethpahli
+             'taphel',              # 27 taphel
+             'ethaphal')            # 28 ethaphal
         ),
-        ('aspect',              # aspect
-            ('n/a',             # 0 n/a
-             'perfect',         # 1 perfect
-             'imperfect',       # 2 imperfect
-             'imperative',      # 3 imperative
-             'infinitive',      # 4 infinitive
-             'participle')      # 5 participle
+        ('aspect',              #  7 aspect
+            ('n/a',                 # 0 n/a
+             'perfect',             # 1 perfect
+             'imperfect',           # 2 imperfect
+             'imperative',          # 3 imperative
+             'infinitive',          # 4 infinitive
+             'participle')          # 5 participle
         ),
-        ('state',               # state
-            ('n/a',             # 0 n/a
-             'absolute',        # 1 absolute
-             'construct',       # 2 construct
-             'emphatic')        # 3 emphatic
+        ('state',               #  8 state
+            ('n/a',                 # 0 n/a
+             'absolute',            # 1 absolute
+             'construct',           # 2 construct
+             'emphatic')            # 3 emphatic
         ),
-        ('number',              # number
-            ('n/a',             # 0 n/a
-             'singular',        # 1 singular
-             'plural')          # 2 plural
+        ('number',              #  9 number
+            ('n/a',                 # 0 n/a
+             'singular',            # 1 singular
+             'plural')              # 2 plural
         ),
-        ('person',              # person
-            ('n/a',             # 0 n/a
-             'first',           # 1 first
-             'second',          # 2 second
-             'third')           # 3 third
+        ('person',              # 10 person
+            ('n/a',                 # 0 n/a
+             'first',               # 1 first
+             'second',              # 2 second
+             'third')               # 3 third
         ),
-        ('gender',              # gender
-            ('n/a',             # 0 n/a
-             'common',          # 1 common
-             'masculine',       # 2 masculine
-             'feminine')        # 3 feminine
+        ('gender',              # 11 gender
+            ('n/a',                 # 0 n/a
+             'common',              # 1 common
+             'masculine',           # 2 masculine
+             'feminine')            # 3 feminine
         ),
-        ('pronoun_type',        # pronoun_type
-            ('n/a',             # 0 n/a
-             'personal',        # 1 personal
-             'demonstrative',   # 2 demonstrative
-             'interrogative')   # 3 interrogative
+        ('pronoun_type',        # 12 pronoun type
+            ('n/a',                 # 0 n/a
+             'personal',            # 1 personal
+             'demonstrative',       # 2 demonstrative
+             'interrogative')       # 3 interrogative
         ),
         ('demonstrative_category',
-                                # demonstrative_category
-            ('n/a',             # 0 n/a
-             'near',            # 1 near
-             'far')             # 2 far
+                                    # 13 demonstrative category
+            ('n/a',                 # 0 n/a
+             'near',                # 1 near
+             'far')                 # 2 far
         ),
-        ('noun_type',           # noun_type
-            ('n/a',             # 0 n/a
-             'propper',         # 1 propper
-             'common')          # 2 common
+        ('noun_type',           # 14 noun type
+            ('n/a',                 # 0 n/a
+             'propper',             # 1 propper
+             'common')              # 2 common
         ),
-        ('numeral_type',        # numeral_type
-            ('n/a',             # 0 n/a
-             'cardinal',        # 1 cardinal
-             'ordinal',         # 2 ordinal
-             'cipher')          # 3 cipher
+        ('numeral_type',        # 15 numeral type
+            ('n/a',                 # 0 n/a
+             'cardinal',            # 1 cardinal
+             'ordinal',             # 2 ordinal
+             'cipher')              # 3 cipher
         ),
-        ('participle_type',     # participle_type
-            ('n/a',             # 0 n/a
-             'active',          # 1 active
-             'passive')         # 2 passive
+        ('participle_type',     # 16 participle type
+            ('n/a',                 # 0 n/a
+             'active',              # 1 active
+             'passive')             # 2 passive
         ),
-        ('grammatical_category',# grammatical_category
-            ('verb',            # 0 verb
-             'participle',      # 1 participle
-             'noun',            # 2 noun
-             'pronoun',         # 3 pronoun
-             'numeral',         # 4 numeral
-             'adjective',       # 5 adjective
-             'particle',        # 6 particle
-             'adverb',          # 7 adverb
-             'idiom')           # 8 idiom
+        ('grammatical_category',# 17 grammatical category
+            ('verb',                # 0 verb
+             'participle',          # 1 participle
+             'noun',                # 2 noun
+             'pronoun',             # 3 pronoun
+             'numeral',             # 4 numeral
+             'adjective',           # 5 adjective
+             'particle',            # 6 particle
+             'adverb',              # 7 adverb
+             'idiom')               # 8 idiom
         ),
-        ('suffix_contraction',  # suffix_contraction
-            ('n/a',             # 0 n/a
-             'suffix',          # 1 suffix
-             'contraction')     # 2 contraction
+        ('suffix_contraction',  # 18 suffix contraction
+            ('n/a',                 # 0 n/a
+             'suffix',              # 1 suffix
+             'contraction')         # 2 contraction
         ),
-        ('suffix_gender',       # suffix_gender
-            ('common OR n/a',   # 0 common OR n/a
-             'masculine',       # 1 masculine
-             'feminine')        # 2 feminine
+        ('suffix_gender',       # 19 suffix gender
+            ('common OR n/a',       # 0 common OR n/a
+             'masculine',           # 1 masculine
+             'feminine')            # 2 feminine
         ),
-        ('suffix_person',       # suffix_person
-            ('n/a',             # 0 n/a
-             'first',           # 1 first
-             'second',          # 2 second
-             'third')           # 3 third
+        ('suffix_person',       # 20 suffix person
+            ('n/a',                 # 0 n/a
+             'first',               # 1 first
+             'second',              # 2 second
+             'third')               # 3 third
         ),
-        ('suffix_number',       # suffix_number
-            ('singular OR n/a', # 0 singular OR n/a
-             'plural')          # 1 plural
+        ('suffix_number',       # 21 suffix number
+            ('singular OR n/a',     # 0 singular OR n/a
+             'plural')              # 1 plural
         ),
-        ('feminine he dot',     # feminine he dot
+        ('feminine he dot',     # 22 feminine he dot
             ('does not have the feminine he dot',
-                                # 0 does not have the feminine he dot
+                                    # 0 does not have the feminine he dot
              'has the feminine he dot')
-                                # 1 has the feminine he dot
+                                    # 1 has the feminine he dot
         )
     )
